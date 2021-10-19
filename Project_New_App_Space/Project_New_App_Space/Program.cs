5e57a08520388daca6ee38ce37402a48d4be485b
DESKTOP-G6IS2T2\Toshiba <marineriscr@gmail.com>
2021-10-19 15:04:10 +0300

    no message

---
+++ Project_New_App_Space/Project_New_App_Space/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_New_App_Space
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello in New_App_Space!!!");
+        }
+    }
+}
